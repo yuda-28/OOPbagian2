d3e58dac53879920c4fd6132791e004b174a7ffe
yuda-28 <rafly.wirayuda@students.amikom.ac.id>
2020-07-02 01:07:12 +0700

    apa

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Inheritance
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person person = new Person("ahmad", 19);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("bayu", 50, "6946", "chemistry");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("ayu", 20, "3301", "ayu97@gmail.com");
+            student.GetNameAndAge();
+        }
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Inheritance
+{
+    public class Teacher : Person
+    {
+        public Teacher(string name, int age, string teacherid, string subject)
+        : base(name, age)
+        {
+            TeacherId = teacherid;
+            Subject = subject;
+        }
+
+        public string TeacherId { get; set; }
+        public string Subject { get; set; }
+
+    }
+}
